02ae686b3a69d80a880abf46af172a8e18af6924
Guslev Ivan <guslev55@gmail.com>
2020-11-24 18:37:01 +0900

    Гуслев Иван СтартАндроид

---
+++ ViewById/src/main/java/com/example/viewbyid/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.viewbyid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        View myTextView = findViewById(R.id.myText);
+
+        Button myBtn = (Button) findViewById(R.id.myBtn);
+        myBtn.setText("My button");
+        myBtn.setEnabled(false);
+
+        CheckBox myChb = (CheckBox) findViewById(R.id.myChb);
+        myChb.setChecked(true);
+    }
+}
